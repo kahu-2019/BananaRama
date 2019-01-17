ed6f12d06ba0e94340e4f49f45ba3d0c92bcfaad
Karl Brocklehurst <kairakau@gmail.com>
2019-01-18 10:36:32 +1300

    modify app.js routes for addNew

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/addNew');
 
 var app = express();
 const fileUpload = require('express-fileupload');
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/addNew', usersRouter);
 
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
@@ -46,7 +46,7 @@ app.get('/ping', function(req, res) {
   res.send('pong');
 });
 
-app.post('/upload', function(req, res) {
+app.post('/addNew', function(req, res) {
   let sampleFile;
   let uploadPath;
 
