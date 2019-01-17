4a9cfff4b907eb627d3f3997f02c309590def429
Karl Brocklehurst <kairakau@gmail.com>
2019-01-17 22:12:37 +1300

    File upload working locally

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const fileUpload = require('express-fileupload');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,9 +24,9 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -36,6 +37,57 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// default file upload options
+// app.use(fileUpload());
+
+// app.post('/upload/', function(req, res) {
+//   if (Object.keys(req.files).length == 0) {
+//     return res.status(400).send('No files were uploaded.');
+//   }
+
+//   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+//   let sampleFile = req.files.sampleFile;
+
+//   // Use the mv() method to place the file somewhere on your server
+//   sampleFile.mv(__dirname + '/public/images/', function(err) {
+//     if (err)
+//       return res.status(500).send(err);
+
+//     res.send('File uploaded!');
+//   });
+// });
+
+// default options
+app.use(fileUpload());
+
+app.get('/ping', function(req, res) {
+  res.send('pong');
+});
+
+app.post('/upload', function(req, res) {
+  let sampleFile;
+  let uploadPath;
+
+  if (Object.keys(req.files).length == 0) {
+    res.status(400).send('No files were uploaded.');
+    return;
+  }
+
+  console.log('req.files >>>', req.files); // eslint-disable-line
+
+  sampleFile = req.files.sampleFile;
+
+  uploadPath = __dirname + '/public/images/' + sampleFile.name;
+
+  sampleFile.mv(uploadPath, function(err) {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    res.send('File uploaded to ' + uploadPath);
+  });
 });
 
 module.exports = app;
